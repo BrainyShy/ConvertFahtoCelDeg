40a1144063a9d35e727a7c2577b6fcceb65843e9
Tiffany <bullardt1988@Tiffanys-MacBook-Pro.local>
2013-10-10 09:14:23 -0700

    Initial Commit

---
+++ ConvertFahtoCelDeg/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  ConvertFahtoCelDeg
+//
+//  Created by Tiffany on 10/10/13.
+//  Copyright (c) 2013 Tiffany. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
