bff5215802091d0a9b32f2fbfa689dd8de13b2be
BrainyShy <bullardt1988@Tiffanys-MacBook-Pro.local>
2013-10-15 12:53:16 -0700

    test

--- ConvertFahtoCelDeg/main.c
+++ ConvertFahtoCelDeg/main.c
@@ -6,13 +6,31 @@
 //  Copyright (c) 2013 Tiffany. All rights reserved.
 //
 
-#include <stdio.h>
+#include <stdio.h> 
+
+float ConvertFtoC(float, float);
 
 int main(int argc, const char * argv[])
 {
-
-    // insert code here...
-    printf("Hello, World!\n");
+    float fahrenheit;
+    float celcius;
+    
+    printf("What is the temperature in Fahrenheit?\n");
+    scanf("%f", &fahrenheit);
+    
+    ConvertFtoC(fahrenheit, celcius);
+    
+    celcius = ConvertFtoC(fahrenheit, celcius);
+    
+    printf(" %.2f degress F is equal to %.2f degrees C\n", fahrenheit, celcius);
+    
+    
     return 0;
 }
 
+float ConvertFtoC(float F, float C)
+{
+    C = (5 * (F - 32)/9);
+
+    return C;
+}
